0058d11a6f6968d7f4374814afb0883ad0fe569b
Gabriel <gabriel.berthel@etu.univ-tours.fr>
2023-04-08 22:45:01 +0200

    header classe personne

--- Personne.h
+++ Personne.h
@@ -1,14 +1,31 @@
-//
-// Created by andre on 08/04/2023.
-//
-
+#include <iostream>
+#include <string>
 #ifndef PROJET_TULLE_PERSONNE_H
 #define PROJET_TULLE_PERSONNE_H
 
+using namespace std;
 
 class Personne {
-
+    static int totalPersonnes;
+    private:
+        int idPersonne;
+        string nom;
+        string premom;
+        string adresse;
+        string telephone;
+    public:
+        Personne(string nom, string prenom, string adresse, string telephone);
+        string getNom();
+        string getPrenom();
+        string getAdresse();
+        string getTelephone();
+        void setNom(string nom);
+        void setPrenom(string prenom);
+        void setAdresse(string adresse);
+        void setTelephone(string telephone);
 };
+
+int Personne::totalPersonnes = 1;
 
 
 #endif //PROJET_TULLE_PERSONNE_H
