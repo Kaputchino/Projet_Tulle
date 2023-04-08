075a2169cf3ea5770b07eb27bde788becba9c8d6
Kaputchino <andreasmulard@gmail.com>
2023-04-08 23:20:26 +0200

    ajout de liste static de sauvegarde

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include "string"
+#include "vector"
+#include "Chauffeur.h"
+#include "Dispatcher.h"
+using namespace std;
+
+static vector<string> listeVille;
+static vector<Dispatcher> listeDispatcher;
+static vector<Chauffeur> listeChauffeur;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
