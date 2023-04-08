2ebeace6b522e76b0e9f2ed6fd7cfcda3f133979
Gabriel <gabriel.berthel@etu.univ-tours.fr>
2023-04-08 23:13:35 +0200

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -1,12 +1,19 @@
 #ifndef PROJET_TULLE_ADMIN_H
 #define PROJET_TULLE_ADMIN_H
 
-
 #include "Personne.h"
+#include "Chauffeur.h"
 
 class Admin : public Personne{
 public:
-    Admin(string nom, string prenom, string adresse, string telephone);
+    Admin();
+    double gainChauffeur(Chauffeur c);
+    int trajetChauffeur(Chauffeur c);
+    int coliesLivree(Chauffeur c);
+    int coliesEnCours(Chauffeur c);
+    int coliesEnAttente(Chauffeur c);
+    int coliesEnValidation(Chauffeur c);
+    int nombreTrajets(Chauffeur c);
 };
 
 
