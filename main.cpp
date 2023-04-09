4be3a6268971777e5bd8d57a0400f6c4a1a8cfb9
Gabriel <gabriel.berthel@etu.univ-tours.fr>
2023-04-09 12:19:48 +0200

    debut test main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,25 @@ static vector<Dispatcher> listeDispatcher;
 static vector<Chauffeur> listeChauffeur;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string villeArriveeA = "Tours";
+    string villeArriveeB = "Paris";
+    
+    listeVille.push_back(villeArriveeA);
+    listeVille.push_back(villeArriveeB);
+
+    Chauffeur chauffeurA = Chauffeur("Perout", "Fred", "105 Rue Constantinople", "0103040506");
+    Chauffeur chauffeurB = Chauffeur("Benito", "Benoit", "3 place Vandome", "0909997867");
+
+    chauffeurA.afficherPersonne();
+    chauffeurB.afficherPersonne();
+
+    Colis colisA = Colis(villeArriveeA, 10.2);
+    Colis colisB = Colis(villeArriveeB, 2.7);
+    Colis colisBA = Colis(villeArriveeB, 3.1);
+    Colis colisBB = Colis(villeArriveeB, 1.2);
+
+    Trajet trajetA = Trajet(chauffeurA.getIdChauffeur(), "Tours", villeArriveeA, "10h20", "13h40", 20, 150);
+    Trajet trajetA = Trajet(chauffeurB.getIdChauffeur(), "Paris", villeArriveeB, "10h20", "13h40", 34, 500);
+
     return 0;
 }
