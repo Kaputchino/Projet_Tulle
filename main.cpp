bb0a38669af89999b859e8e4c20b730812d9d433
Gabriel <gabriel.berthel@etu.univ-tours.fr>
2023-04-11 17:36:11 +0200

    fixed random generation

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@
 #include "common.h"
 using namespace std;
 
-// vector<string> listeVille = {"Tours","St-Pierre-Des-Corps","Tulle","Valence","Nouméa","Arachon","Casablanca","Bonifacio","Colmar","Montparnasse","Narbonne","Strasbourg","Brumath","Hoerdt","Haguenau","Mulhouse","Breuschwickersheim","Illkirch-Graffenstaden","Oberschaeffolsheim","Souffelweyersheim","Breuschwickersheim","Geispolsheim","Agullana","Lake Mary","Milwaukee"};
-vector<string> listeVille = {"Tours","St-Pierre-Des-Corps","Tulle","Valence","Nouméa"};
+vector<string> listeVille = {"Tours","St-Pierre-Des-Corps","Tulle","Valence","Nouméa","Arachon","Casablanca","Bonifacio","Colmar","Montparnasse","Narbonne","Strasbourg","Brumath","Hoerdt","Haguenau","Mulhouse","Breuschwickersheim","Illkirch-Graffenstaden","Oberschaeffolsheim","Souffelweyersheim","Breuschwickersheim","Geispolsheim","Agullana","Lake Mary","Milwaukee"};
 vector<Dispatcher *> listeDispatcher;
 vector<Chauffeur *> listeChauffeur;
 
 int main(int argc, char const *argv[]) {
+    srand((unsigned) time(0)); // seeding for random number generation
+
     string villeArriveeA = "Tours";
     string villeArriveeB = "Paris";
 
