6b25d95d6d601c4ab99716ed6a151c221549202f
Gabey <gabriel.berthel@etu.univ-tours.fr>
2023-04-19 18:04:54 +0200

    remove conf.h

--- shared/conf.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROJET_TULLE_CONF_H
-#define PROJET_TULLE_CONF_H
-#include <QString>
-
-inline const QString path_to_main_db = "C:/Users/Gabey/CLionProjects/Projet_Tulle/db/database.db";
-
-#endif //PROJET_TULLE_CONF_H
