4cd9f17cca0e58ad19ea2785087ba6b4db2d2971
Kaputchino <andreasmulard@gmail.com>
2023-04-19 15:09:59 +0200

    nique le sql

--- applications/mainwindow.cpp
+++ applications/mainwindow.cpp
@@ -3,6 +3,9 @@
 #include <QPushButton>
 #include <string>
 #include <vector>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
 
 using namespace std;
 
@@ -12,6 +15,33 @@ int main(int argc, char** argv)
 
     QPushButton button("Hello world !");
     button.show();
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("projet_tulle");
+    db.setUserName("root");
+    db.setPort(3306);
+    db.setPassword("");
+    if(!db.open())
+    {
+        cout << "Can't Connect to DB !";
+    }
+    else
+    {
+        cout << "Connected Successfully to DB !";
+        QSqlQuery query;
+        query.prepare("QUERY TO BE SENT TO THE DB");
+        if(!query.exec())
+        {
+            cout << "Can't Execute Query !";
+        }
+        else
+        {
+            cout << "Query Executed Successfully !";
+        }
+    }
+
+
 
     return app.exec();
 }
