1c629ee57d861694636697b3f85d52d8afe4b4ec
Gabey <gabriel.berthel@etu.univ-tours.fr>
2023-04-19 15:37:12 +0200

    push

--- applications/mainwindow.cpp
+++ applications/mainwindow.cpp
@@ -2,9 +2,14 @@
 #include <QApplication>
 #include <QPushButton>
 #include <string>
+#include "shared/common.h"
+#include "shared/conf.h"
 #include <vector>
+#include <filesystem>
 
 using namespace std;
+
+QSqlDatabase db;
 
 int main(int argc, char** argv)
 {
@@ -12,6 +17,18 @@ int main(int argc, char** argv)
 
     QPushButton button("Hello world !");
     button.show();
+
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(path_to_main_db);
+
+    if (!db.open())
+    {
+        qDebug() << "Error: connection with database failed";
+    }
+    else
+    {
+        qDebug() << "Database: connection ok";
+    }
 
     return app.exec();
 }
