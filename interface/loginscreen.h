301e1253e7349450a023ae94f3c054eea49a07f6
Gabey <gabriel.berthel@etu.univ-tours.fr>
2023-04-20 22:36:22 +0200

    proper use of connect

--- interface/loginscreen.h
+++ interface/loginscreen.h
@@ -19,9 +19,9 @@ private:
     Ui::LoginScreen *ui;
 
 private slots:
-    void on_connectionButton_clicked();
-    void on_quitButton_clicked();
-    void on_resetButton_clicked();
+    void connectClicked();
+    void quitClicked();
+    void resetClicked();
 };
 
 #endif // LOGINSCREEN_H
