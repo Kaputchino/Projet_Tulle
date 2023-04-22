d141b9e4c2d8c6f9e47ecebc4b6c8bda033244d6
Gabey <gabriel.berthel@etu.univ-tours.fr>
2023-04-22 22:38:48 +0200

    Revert "remove dead code"
    This reverts commit f157267ba13a82de145b50ffa641a8c7cafda885.

--- core/sources/Dispatcher.cpp
+++ core/sources/Dispatcher.cpp
@@ -140,6 +140,33 @@ vector<Colis *> Dispatcher::loadColisOfDispatcherFromDB(){
 
     return list;
 }
+Dispatcher *Dispatcher::findDispatcherById(int id) {
+    QSqlQuery query;
+    query.prepare( "SELECT * FROM personne WHERE idPersonne = :id and role = :role");
+    query.bindValue(":id", QVariant(id));
+    query.bindValue(":role", QString::fromStdString(ROLE_DISPATCHER));
+
+    if(!query.exec() ){
+        qDebug() << query.lastError();
+        throw std::runtime_error("Erreur critique lors d'une requete");
+    }
+
+    if(query.next()){
+        cout << "qwdqw" << endl;
+        int idPersonne = query.value( 0 ).toInt();
+        string nom = query.value(1).toString().toStdString();
+        string prenom = query.value(2).toString().toStdString();
+        string adresse = query.value(3).toString().toStdString();
+        string email = query.value(4).toString().toStdString();
+        string password = query.value(5).toString().toStdString();
+        string role = query.value(6).toString().toStdString();
+        unique_ptr<Dispatcher> p = std::make_unique<Dispatcher>(idPersonne,nom,prenom,adresse,email,password);
+        return p.get();
+    } else {
+        Errors::appendError("Pas d'utilisateur avec l'id: " + to_string(id));
+    }
+    return nullptr;
+}
 
 vector<Colis *> Dispatcher::getListColis() {
     return listeColis;
