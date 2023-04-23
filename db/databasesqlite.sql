ef3744bbb5ab7a8d1d289c4c87e05ba684e1839b
Gabey <gabriel.berthel@etu.univ-tours.fr>
2023-04-23 18:10:53 +0200

    adding default account

--- db/databasesqlite.sql
+++ db/databasesqlite.sql
@@ -2,38 +2,39 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS "personne";
 CREATE TABLE IF NOT EXISTS "personne" (
 										  "idPersonne"	INTEGER NOT NULL,
-										  "nom"	TEXT NOT NULL,
-										  "prenom"	TEXT NOT NULL,
-										  "adresse"	TEXT NOT NULL,
-										  "email"	TEXT NOT NULL,
-										  "password"	TEXT NOT NULL,
-										  "role"	TEXT NOT NULL,
-										  CONSTRAINT "personne_PK" PRIMARY KEY("idPersonne")
-	);
-
-DROP TABLE IF EXISTS "trajet";
-CREATE TABLE IF NOT EXISTS "trajet" (
-										"idTrajet"	INTEGER NOT NULL,
-										"villeDepart"	TEXT NOT NULL,
-										"villeArrivee"	TEXT NOT NULL,
-										"horaireDepart"	TEXT NOT NULL,
-										"horaireArrivee"	TEXT NOT NULL,
-										"poids"	REAL NOT NULL,
-										"prix"	REAL NOT NULL,
-										"statut"	INTEGER NOT NULL,
-										"idChauffeur"	INTEGER,
-										CONSTRAINT "trajet_chauffeur_FK" FOREIGN KEY("idChauffeur") REFERENCES "personne"("idPersonne")
+										  "nom"	Varchar(50) NOT NULL,
+	"prenom"	Varchar(50) NOT NULL,
+	"adresse"	Varchar(250) NOT NULL,
+	"email"	Varchar(50) NOT NULL,
+	"password"	Varchar(264) NOT NULL,
+	"role"	Varchar(264) NOT NULL,
+	CONSTRAINT "personne_PK" PRIMARY KEY("idPersonne" AUTOINCREMENT)
 	);
 DROP TABLE IF EXISTS "colis";
 CREATE TABLE IF NOT EXISTS "colis" (
 									   "idColis"	INTEGER NOT NULL,
-									   "poids"	REAL NOT NULL,
-									   "villeArivee"	TEXT NOT NULL,
-									   "dataAjout"	TEXT NOT NULL,
-									   "statut"	INTEGER NOT NULL,
-									   "idTrajet"	INTEGER,
-                                       "idDispatcher"	INTEGER,
-									   CONSTRAINT "colis_PK" PRIMARY KEY("idColis")
-	CONSTRAINT "trajet_colis_FK" FOREIGN KEY("idTrajet") REFERENCES "trajet"("idTrajet")
-    CONSTRAINT "dispatcher_colis_FK" FOREIGN KEY("idDispatcher") REFERENCES "personne"("idPersonne")
+									   "poids"	Double NOT NULL,
+									   "villeArivee"	Varchar(264) NOT NULL,
+	"dataAjout"	Varchar(50) NOT NULL,
+	"statut"	Smallint NOT NULL,
+	"idTrajet"	Int,
+	"idDispatcher"	INTEGER,
+	CONSTRAINT "colis_PK" PRIMARY KEY("idColis" AUTOINCREMENT),
+	FOREIGN KEY("idDispatcher") REFERENCES "personne"("idPersonne"),
+	CONSTRAINT "colis_trajet_FK" FOREIGN KEY("idTrajet") REFERENCES "trajet"("idTrajet")
 	);
+DROP TABLE IF EXISTS "trajet";
+CREATE TABLE IF NOT EXISTS "trajet" (
+										"idTrajet"	INTEGER NOT NULL,
+										"villeDepart"	Varchar(264) NOT NULL,
+	"villeArrivee"	Varchar(264) NOT NULL,
+	"horaireDepart"	Varchar(264) NOT NULL,
+	"horaireArrivee"	Varchar(264) NOT NULL,
+	"poids"	Double NOT NULL,
+	"prix"	Double NOT NULL,
+	"statut"	Smallint NOT NULL,
+	"idPersonne"	Int,
+	CONSTRAINT "trajet_PK" PRIMARY KEY("idTrajet" AUTOINCREMENT),
+	CONSTRAINT "trajet_personne_FK" FOREIGN KEY("idPersonne") REFERENCES "personne"("idPersonne")
+	);
+COMMIT;
